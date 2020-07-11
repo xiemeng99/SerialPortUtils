b3ee0779a7f7156b4d82374528da7d9fa31eb029
Shanyaliux <445502929@qq.com>
2020-07-11 20:36:42 +0800

    Create SerialPortUtil

---
+++ SerialPortUtils/src/main/java/world/shanya/serialportutils/SerialPort.kt
@@ -0,0 +1,11 @@
+package world.shanya.serialportutils
+
+/**
+ * @ClassName SerialPort
+ * @Description TODO
+ * @Author Shanya
+ * @Date 2020/7/11 20:33
+ * @Version 1.0.0
+ **/
+class SerialPort {
+}
---
+++ app/src/main/java/world/shanya/serialportutil/MainActivity.kt
@@ -0,0 +1,13 @@
+package world.shanya.serialportutil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
