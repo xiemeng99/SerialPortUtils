2848c8e0cc0c49745e92834f36d1b38aabb913de
Shanyaliux <445502929@qq.com>
2020-07-12 17:25:57 +0800

    ReadData Finished
    性能上还可以进一步优化，待下回处理

--- SerialPortUtils/src/main/java/world/shanya/serialportutils/SerialPort.kt
+++ SerialPortUtils/src/main/java/world/shanya/serialportutils/SerialPort.kt
@@ -8,13 +8,17 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.Build
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import java.io.IOException
 import java.io.InputStream
+import java.nio.charset.StandardCharsets
+
 
 /**
  * @ClassName SerialPort
@@ -49,6 +53,7 @@ class SerialPort private constructor(context: Context) {
     //各种状态回调
     private var scanStatusCallback:ScanStatusCallback ?= null
     private var connectionCallback:ConnectionCallback ?= null
+    private var readDataCallback: ReadDataCallback ?= null
 
     //SPP服务UUID号
     private val UUID = "00001101-0000-1000-8000-00805F9B34FB"
@@ -59,6 +64,8 @@ class SerialPort private constructor(context: Context) {
     private lateinit var inputStream: InputStream
     val pairedDevicesArrayAdapter:ArrayAdapter<String> = ArrayAdapter(context,R.layout.device_name)
     val unPairedDevicesArrayAdapter:ArrayAdapter<String> = ArrayAdapter(context,R.layout.device_name)
+
+    private var readThreadStarted = false
 
     /**
     * 蓝牙设配器各种状态的广播监听器
@@ -174,7 +181,10 @@ class SerialPort private constructor(context: Context) {
                 ).show()
                 inputStream = bluetoothSocket.inputStream
                 //打开接收线程
-
+                if (!readThreadStarted){
+                    readThread.start()
+                    readThreadStarted = true
+                }
             }catch (e:IOException){
                 connectionCallback?.connectionResult(false)
                 Toast.makeText(context,context.getString(R.string.connection_failed),Toast.LENGTH_SHORT).show()
@@ -212,5 +222,55 @@ class SerialPort private constructor(context: Context) {
 
     fun getConnectionResult(connectionCallback: ConnectionCallback){
         this.connectionCallback = connectionCallback
+    }
+
+    /**
+    * 接收数据的线程
+    * @Author Shanya
+    * @Date 2020/7/12 17:18
+    * @Version 1.0.0
+    */
+    private val readThread = object :Thread(){
+        @RequiresApi(Build.VERSION_CODES.KITKAT)
+        override fun run() {
+            super.run()
+            var len = 0;
+            var buffer = ByteArray(0)
+            var s = ""
+            var flag = false
+            while (true){
+                if (!readThreadStarted) {
+                    return
+                }
+                sleep(100)
+                len = inputStream.available()
+                while (len != 0){
+                    flag = true
+                    buffer = ByteArray(len)
+                    inputStream.read(buffer)
+                    sleep(10)
+                    len = inputStream.available()
+                }
+                if (flag){
+                    s = String(buffer,StandardCharsets.UTF_8)
+                    readDataCallback?.readData(s)
+                    flag = false
+                }
+            }
+        }
+    }
+
+    /**
+    * 接收数据的回调
+    * @Author Shanya
+    * @Date 2020/7/12 17:19
+    * @Version 1.0.0
+    */
+    interface ReadDataCallback{
+        fun readData(data: String)
+    }
+
+    fun getReadData(readDataCallback: ReadDataCallback){
+        this.readDataCallback = readDataCallback
     }
 }
--- app/src/main/java/world/shanya/serialportutil/MainActivity.kt
+++ app/src/main/java/world/shanya/serialportutil/MainActivity.kt
@@ -3,7 +3,10 @@ package world.shanya.serialportutil
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import world.shanya.serialportutils.SearchActivity
 import world.shanya.serialportutils.SerialPort
 
@@ -15,6 +18,14 @@ class MainActivity : AppCompatActivity() {
         val serialPort = SerialPort.getInstance(this)
 
         val serialPort1 = SerialPort.getInstance(this)
+
+        serialPort.getReadData(object :SerialPort.ReadDataCallback{
+            override fun readData(data: String) {
+                MainScope().launch {
+                    Toast.makeText(this@MainActivity,data,Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
 
         button.setOnClickListener {
             println(serialPort)
