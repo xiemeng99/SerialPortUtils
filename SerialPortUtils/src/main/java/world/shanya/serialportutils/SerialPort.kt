cfb81ff1116690a74846759032755319cdd5e487
Shanyaliux <445502929@qq.com>
2020-07-12 16:35:09 +0800

    Search and Connect finished
    内部集成搜索页面
    连接中断的监听

--- SerialPortUtils/src/main/java/world/shanya/serialportutils/SerialPort.kt
+++ SerialPortUtils/src/main/java/world/shanya/serialportutils/SerialPort.kt
@@ -1,11 +1,216 @@
 package world.shanya.serialportutils
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import java.io.IOException
+import java.io.InputStream
+
 /**
  * @ClassName SerialPort
- * @Description TODO
+ * @Description 蓝牙串口工具类
  * @Author Shanya
  * @Date 2020/7/11 20:33
  * @Version 1.0.0
  **/
-class SerialPort {
+class SerialPort private constructor(context: Context) {
+
+    //懒汉式SingleTon
+    companion object {
+        private var instance: SerialPort ?= null
+        fun getInstance(context: Context): SerialPort{
+            val temp = instance
+            if (temp != null){
+                return temp
+            }
+            return synchronized(this) {
+                val temp2 = instance
+                if (temp2 != null) {
+                    temp2
+                } else {
+                    val created = SerialPort(context.applicationContext)
+                    instance = created
+                    created
+                }
+            }
+        }
+    }
+
+    //各种状态回调
+    private var scanStatusCallback:ScanStatusCallback ?= null
+    private var connectionCallback:ConnectionCallback ?= null
+
+    //SPP服务UUID号
+    private val UUID = "00001101-0000-1000-8000-00805F9B34FB"
+
+    //获取蓝牙设配器
+    private val bluetoothAdapter:BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    private lateinit var bluetoothSocket:BluetoothSocket
+    private lateinit var inputStream: InputStream
+    val pairedDevicesArrayAdapter:ArrayAdapter<String> = ArrayAdapter(context,R.layout.device_name)
+    val unPairedDevicesArrayAdapter:ArrayAdapter<String> = ArrayAdapter(context,R.layout.device_name)
+
+    /**
+    * 蓝牙设配器各种状态的广播监听器
+    * @Author Shanya
+    * @Date 2020/7/12 16:33
+    * @Version 1.0.0
+    */
+    private val searchReceiver = object :BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val action = intent?.action
+            if (BluetoothDevice.ACTION_FOUND == action){
+                val device:BluetoothDevice ?= intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                if (device != null){
+                    if (device.bondState != BluetoothDevice.BOND_BONDED) {
+                        unPairedDevicesArrayAdapter.add("${device.name}\n${device.address}")
+                    }
+                }
+            }else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED == action){
+                //搜索完成
+                scanStatusCallback?.scanStatus(false)
+                if (unPairedDevicesArrayAdapter.count == 0){
+                    //没有搜索到设备
+                    unPairedDevicesArrayAdapter.add(context?.getString(R.string.no_available))
+                }
+            }else if (BluetoothDevice.ACTION_ACL_DISCONNECTED == action){
+                val device:BluetoothDevice ?= intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                connectionCallback?.connectionResult(false)
+                Toast.makeText(context,"${device?.name}  ${context?.getString(R.string.disconnect)}",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    init {
+        context.registerReceiver(searchReceiver,IntentFilter(BluetoothDevice.ACTION_FOUND))
+        context.registerReceiver(searchReceiver,IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED))
+        context.registerReceiver(searchReceiver,IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED))
+        context.registerReceiver(searchReceiver,IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED))
+        val pairedDevices:Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
+        if (pairedDevices.isNotEmpty()){
+            for (device in pairedDevices){
+                pairedDevicesArrayAdapter.add("${device.name}\n${device.address}")
+            }
+        }else {
+            pairedDevicesArrayAdapter.add(context.getString(R.string.no_paired))
+        }
+    }
+
+    /**
+    * 打开搜索页面
+    * @Author Shanya
+    * @Date 2020/7/12 16:30
+    * @Version 1.0.0
+    */
+    fun openSearchPage(context: Context){
+        context.startActivity(Intent(context,SearchActivity::class.java))
+    }
+
+    /**
+    * 开始搜索可用设备
+    * @Author Shanya
+    * @Date 2020/7/12 16:31
+    * @Version 1.0.0
+    */
+    fun doDiscovery(context: Context){
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(context,context.getString(R.string.no_permission),Toast.LENGTH_SHORT).show()
+        }else{
+            if (!bluetoothAdapter.isEnabled){
+                Toast.makeText(context,context.getString(R.string.open_bluetooth),Toast.LENGTH_SHORT).show()
+                return
+            }
+            if (bluetoothAdapter.isDiscovering){
+                bluetoothAdapter.cancelDiscovery()
+                scanStatusCallback?.scanStatus(false)
+            }
+            unPairedDevicesArrayAdapter.clear()
+            bluetoothAdapter.startDiscovery()
+            scanStatusCallback?.scanStatus(true)
+        }
+    }
+
+    /**
+    * 搜索列表项的先择监听
+    * @Author Shanya
+    * @Date 2020/7/12 16:31
+    * @Version 1.0.0
+    */
+    val devicesClickListener =
+        AdapterView.OnItemClickListener { parent, view, position, id ->
+            if (!bluetoothAdapter.isEnabled){
+                Toast.makeText(context,context.getString(R.string.open_bluetooth),Toast.LENGTH_SHORT).show()
+                return@OnItemClickListener
+            }
+            val info = (view as TextView).text.toString()
+            if (info == context.getString(R.string.no_paired) || info == context.getString(R.string.no_available)) {
+                return@OnItemClickListener
+            }
+            bluetoothAdapter.cancelDiscovery()
+            val address = info.substring(info.length - 17)
+            val bluetoothDevice = bluetoothAdapter.getRemoteDevice(address)
+            try {
+                bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(java.util.UUID.fromString(UUID))
+                bluetoothSocket.connect()
+                connectionCallback?.connectionResult(true)
+                Toast.makeText(
+                    context,
+                    "${bluetoothDevice.name}  ${context.getString(R.string.connect_success)}",
+                    Toast.LENGTH_SHORT
+                ).show()
+                inputStream = bluetoothSocket.inputStream
+                //打开接收线程
+
+            }catch (e:IOException){
+                connectionCallback?.connectionResult(false)
+                Toast.makeText(context,context.getString(R.string.connection_failed),Toast.LENGTH_SHORT).show()
+                try {
+                    bluetoothSocket.close()
+                }catch (e:IOException){
+                    e.printStackTrace()
+                }
+            }
+        }
+
+    /**
+    * 搜索状态的回调
+    * @Author Shanya
+    * @Date 2020/7/12 16:32
+    * @Version 1.0.0
+    */
+    interface ScanStatusCallback{
+        fun scanStatus(status: Boolean)
+    }
+
+    fun getScanStatus(scanStatusCallback: ScanStatusCallback){
+        this.scanStatusCallback = scanStatusCallback
+    }
+
+    /**
+    * 连接状态的回调
+    * @Author Shanya
+    * @Date 2020/7/12 16:32
+    * @Version 1.0.0
+    */
+    interface ConnectionCallback{
+        fun connectionResult(result: Boolean)
+    }
+
+    fun getConnectionResult(connectionCallback: ConnectionCallback){
+        this.connectionCallback = connectionCallback
+    }
 }
--- app/src/main/java/world/shanya/serialportutil/MainActivity.kt
+++ app/src/main/java/world/shanya/serialportutil/MainActivity.kt
@@ -1,13 +1,29 @@
 package world.shanya.serialportutil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import world.shanya.serialportutils.SearchActivity
+import world.shanya.serialportutils.SerialPort
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val serialPort = SerialPort.getInstance(this)
 
+        val serialPort1 = SerialPort.getInstance(this)
+
+        button.setOnClickListener {
+            println(serialPort)
+            println(serialPort1)
+        }
+
+        button2.setOnClickListener {
+//            startActivity(Intent(this,SearchActivity::class.java))
+            serialPort.openSearchPage(this)
+        }
     }
 }
