04ce0bb817324ebe35147d910343ca84d7a05c95
Shanyaliux <445502929@qq.com>
2020-08-21 10:53:58 +0800

    V1.0.0 finished
    十六进制发送接收完善
    内部自带十六进制输入检查
    重构部分代码
    搜索页面重新设计
    使用时申请权限
    暂时移除按键监听器
    依赖简洁化

--- app/src/main/java/world/shanya/serialportutil/MainActivity.kt
+++ app/src/main/java/world/shanya/serialportutil/MainActivity.kt
@@ -1,35 +1,15 @@
 package world.shanya.serialportutil
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import world.shanya.serialportutils.PermissionActivity
 import world.shanya.serialportutils.SerialPort
-import world.shanya.serialportutils.SerialPortOld
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val serialPort = SerialPort.getInstance(this)
-
-        button.setOnClickListener {
-            serialPort.openSearchPage(this)
-        }
-
-        button2.setOnClickListener {
-            serialPort.disconnect()
-        }
-
-        serialPort.getReadData {
-            println(it)
-        }
 
 
     }
